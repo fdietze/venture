20287fcf21bcd4c96770413ad778bf71ef1925b2
Felix Dietze <felix@beerleader.net>
2012-10-17 01:48:10 +0200

    seeded random number generator deterministic generation maxDegree

--- levelgenerator/LevelGenerator.scala
+++ levelgenerator/LevelGenerator.scala
@@ -10,9 +10,10 @@ import java.io.File
 
 object LevelGenerator extends App {
 
-  for( i <- 0 until 300 ) {
-    val graph = new OverviewGraph
+  for( i <- 0 until 30 ) {
+    val graph = new OverviewGraph(i)
     println("Drawing Graph %3d..." format i)
+    (new File("out")).mkdirs()
     graph.drawToImage("out/test%03d.png" format i)
   }
   
@@ -24,10 +25,10 @@ object Config {
   val width = 500
   val height = 500
   val dungeonCount = 15
-  //TODO: val maxDegree = 4
+  val maxDegree = 4
   val minDistance = 90
   val edgeProbabilities = Array(0.8,0.5,0.2)
-  val minLineNodeDistance = 60
+  val minLineNodeDistance = 60 //TODO: statt Pixelabstand, den Winkel vom erzeugten Dreieck limitieren
   
   val nodeDiameter = 40
 }
@@ -64,68 +65,61 @@ case class Line(start:Vec2, end:Vec2) {
   }
   
   def distance(p:Vec2) = {
-  	val v = dir.normalized
-  	val t = (p-start) dot v
-  	if( 0 < t && t < length )
-  		(apply(t/length) - p).length
-  	else
-  		Double.MaxValue
+    val v = dir.normalized
+    val t = (p-start) dot v
+    if( 0 < t && t < length )
+      (apply(t/length) - p).length
+    else
+      Double.MaxValue
   }
 }
 
-class OverviewGraph {
+class OverviewGraph(seed:Long) {
   import Config._
   import math._
-  case class Node(x:Int, y:Int, var difficulty:Int = 0) {
-    def neighbours:Set[Node] = ways.map(_.set).filter(_ contains this).map(s => (s - this).head)
+  
+  private val rng = new util.Random(seed)
+  
+  case class Node(x:Int, y:Int) {
+    var difficulty:Int = 0
+    def neighbours = ways.filter(_ contains this).map(_ otherNode this)
     def degree = neighbours.size
     def point = Vec2(x,y)
   }
+
   case class Edge(nA:Node, nB:Node) {
-    def set = Set(nA, nB)
+    def contains(n:Node) = (n == nA || n == nB)
+    def otherNode(n:Node) = { require(contains(n)); if( n == nA) nB else nA }
     def line = Line(Vec2(nA.x, nA.y), Vec2(nB.x, nB.y))
     def intersects(that:Edge) = this.line intersects that.line
   }
-  var dungeons:List[Node] = Nil
-  var ways:Set[Edge] = Set.empty
+  
+  var ways:List[Edge] = Nil
   var dependencies:List[Edge] = Nil
   
-  def r = util.Random.nextInt.abs
-  def randomDungeon = dungeons(r % dungeons.size)
+  private def rInt = rng.nextInt & Int.MaxValue
+  private def rDouble = rng.nextDouble
+  
+  def randomDungeon = dungeons(rInt % dungeons.size)
   def nodeDistance(n1:Node, n2:Node) = sqrt(pow(n1.x-n2.x,2)+pow(n1.y-n2.y,2))
-  
-  // Knoten 
-  for( i <- 0 until dungeonCount ) {
-    var newNode:Node = null
-    do {
-      newNode = Node(r % width, r % height)
-    } while(
-      dungeons.exists(d => nodeDistance(d, newNode) < minDistance) ||
-      newNode.x-nodeDiameter/2 < 0 ||
-      newNode.x+nodeDiameter/2 > width ||
-      newNode.y-nodeDiameter/2 < 0 ||
-      newNode.y+nodeDiameter/2 > height
-    )
-    dungeons ::= newNode
-  }
-  
+
   def isConnected = {
-    var visited = Set.empty[Node]
-    var next = Set(dungeons.head)
+    var visited:List[Node] = Nil
+    var next = dungeons.take(1)
     while( next.nonEmpty ) {
       val current = next.head
-      next -= current
+      next = next.tail
       if( !(visited contains current) ) {
-        visited += current
-        next ++= current.neighbours
+        visited ::= current
+        next :::= current.neighbours
       }
     }
     visited.size == dungeons.size
   }
   
   def hasCycle = {
-    var visited = Set.empty[Node]
-    var finished = Set.empty[Node]
+    var visited:List[Node] = Nil
+    var finished:List[Node] = Nil
     var foundCycle = false
     for( node <- dungeons ) {
       dfs(node, node)
@@ -138,13 +132,32 @@ class OverviewGraph {
         foundCycle = true
         return
       }
-      visited += v
+      visited ::= v
       for( w <- v.neighbours if(w != source) )
         dfs(w, v)
-      finished += v
+      finished ::= v
     }
     
     foundCycle
+  }
+
+  
+  // Knoten 
+  var dungeons:List[Node] = Nil
+  for( i <- 0 until dungeonCount ) {
+    var newNode:Node = null
+    do {
+      newNode = Node(rInt % width, rInt % height)
+    } while(
+      dungeons.exists(d =>
+        nodeDistance(d, newNode) < minDistance
+        ) ||
+      newNode.x-nodeDiameter/2 < 0 ||
+      newNode.x+nodeDiameter/2 > width ||
+      newNode.y-nodeDiameter/2 < 0 ||
+      newNode.y+nodeDiameter/2 > height
+    )
+    dungeons ::= newNode
   }
   
   val startDungeon = randomDungeon
@@ -154,30 +167,24 @@ class OverviewGraph {
   while(L.nonEmpty) {
     val e = L.head
     L = L.tail
-    ways += e
+    ways ::= e
     if(hasCycle)
-      ways -= e
+      ways = ways.tail
   }
   
   // Weitere Kanten
   for( node <- dungeons ) {
     val d1 = node
     val close = dungeons.sortBy(d => nodeDistance(d1,d)).tail zip edgeProbabilities
-    val d2s = close.filter(util.Random.nextDouble <= _._2).map(_._1)
-    for( way <- d2s.map(d2 => Edge(d1,d2)) ) {
-    	if( !ways.exists(_.intersects(way) ) &&
-	    	!dungeons.filterNot(d => d == way.nA || d == way.nB).exists{d => way.line.distance(d.point) < Config.minLineNodeDistance}
-    	 )
-	    	ways += way
+    val d2s = close.filter(rDouble <= _._2).map(_._1)
+    for( (way,d2) <- d2s.map(d2 => (Edge(d1,d2),d2)) ) {
+      if( !ways.exists(_.intersects(way) ) &&
+        !dungeons.filterNot(d => d == way.nA || d == way.nB).exists{d => way.line.distance(d.point) < Config.minLineNodeDistance} &&
+        d1.degree <= maxDegree && d2.degree <= maxDegree
+       )
+        ways ::= way
     }
   }
-/*    val d1 = connected(r % connected.size)
-    //val d2 = dungeons.sortBy(d => nodeDistance(d1,d)).drop(1).zip(edgeProbabilities)
-    val close = unconnected.sortBy(d => nodeDistance(d1,d)) zip edgeProbabilities.scan(0)(_+_).tail
-    val random = r % edgeProbabilities.sum
-    val d2 = close.find(random < _._2).get._1
-    connected ::= d2
-    ways += Edge(d1,d2)*/
   
   
   def drawToImage(filename:String) {
