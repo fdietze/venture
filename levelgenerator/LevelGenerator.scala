aa39d4c1ec07179f88b969202778220fbe5939b2
Felix Dietze <felix@beerleader.net>
2012-10-20 18:57:28 +0200

    Abstracted Graph and Geometry Types

--- levelgenerator/LevelGenerator.scala
+++ levelgenerator/LevelGenerator.scala
@@ -34,200 +34,30 @@ object Config {
   val branchDiameter = 40
 }
 
-case class Vec2(x:Double, y:Double) {
-  import math._
-  def +(that:Vec2) = Vec2(this.x + that.x, this.y + that.y)
-  def -(that:Vec2) = Vec2(this.x - that.x, this.y - that.y)
-  def *(that:Double) = Vec2(this.x * that, this.y * that)
-  def /(that:Double) = Vec2(this.x / that, this.y / that)
-  def dot(that:Vec2) = this.x * that.x + this.y * that.y
-  def length = sqrt(x*x+y*y)
-  def normalized = this / length
-}
-
-case class Line(start:Vec2, end:Vec2) {
-  def dir = end-start
-  def normal = Vec2(dir.y,-dir.x)
-  def apply(t:Double) = start + dir*t
-  def length = dir.length
-  def intersection(that:Line) = {
-    // t = (A-P).n / (A-B).n
-    val d = this.dir dot that.normal
-    if( d == 0 ) None
-    else Some( ((that.end - this.start) dot that.normal)/d )
-  }
-  
-  def ccw(p:Vec2) = (start.x - p.x) * (end.y - p.y) - (start.y - p.y) * (end.x - p.x) > 0
-
-  def intersects(that:Line) = {
-    (this.intersection(that), that.intersection(this)) match {
-      case (Some(s),Some(t)) =>
-        0 < t && t < 1 && 0 < s && s < 1
-      case _ => false
-    }
-  }
-  
-  def distance(p:Vec2) = {
-    val v = dir.normalized
-    val t = (p-start) dot v
-    if( 0 < t && t < length )
-      (apply(t/length) - p).length
-    else
-      Double.MaxValue
-  }
-}
-
 class Dungeon(seed:Any) {
   import Config._
-  import math._
+  import graph._
+  import geometry._
   
   private val rng = new util.Random(seed.hashCode)
   
-  case class Branch(x:Int, y:Int, seed:Int) {
+  case class Branch(point:Vec2, seed:Int) extends PointVertex {
     var id:Int = 0
-    def neighbours:List[Branch] = neighbours(connections)
-    def neighbours(edges:List[BranchConnection] = connections) = edges.filter(_ contains this).map(_ otherBranch this).distinct
-    def degree = neighbours.size
-    def point = Vec2(x,y)
     override def toString = "Branch(%d)" format id
   }
 
-  case class BranchConnection(nA:Branch, nB:Branch) {
-    def contains(n:Branch) = (n == nA || n == nB)
-    def otherBranch(n:Branch) = { require(contains(n)); if( n == nA) nB else nA }
-    def line = Line(Vec2(nA.x, nA.y), Vec2(nB.x, nB.y))
-    def intersects(that:BranchConnection) = this.line intersects that.line
-  }
+  case class BranchConnection(nA:Branch, nB:Branch) extends PointEdge
   
   var branches:List[Branch] = Nil
   var connections:List[BranchConnection] = Nil
-//  var dependencies:List[BranchConnection] = Nil
   
   private def rInt = rng.nextInt & Int.MaxValue
   private def rDouble = rng.nextDouble
   
   def randomBranch = branches(rInt % branches.size)
-  def branchDistance(n1:Branch, n2:Branch) = sqrt(pow(n1.x-n2.x,2)+pow(n1.y-n2.y,2))
-
-  def isConnected = {
-    var visited:List[Branch] = Nil
-    var next = branches.take(1)
-    while( next.nonEmpty ) {
-      val current = next.head
-      next = next.tail
-      if( !(visited contains current) ) {
-        visited ::= current
-        next :::= current.neighbours
-      }
-    }
-    visited.size == branches.size
-  }
-  
-  def hasCycle = {
-    var visited:List[Branch] = Nil
-    var finished:List[Branch] = Nil
-    var foundCycle = false
-    for( branch <- branches ) {
-      dfs(branch, branch)
-    }
-    
-    def dfs(v:Branch, source:Branch) {
-      if(finished contains v)
-        return
-      if((visited contains v)) {
-        foundCycle = true
-        return
-      }
-      visited ::= v
-      for( w <- v.neighbours if(w != source) )
-        dfs(w, v)
-      finished ::= v
-    }
-    
-    foundCycle
-  }
 
 
-  def delaunayEdges = {
-    var edges:List[BranchConnection] = Nil
-    // initial triangle mesh
-    for( branch <- branches ) {
-      val closest = branches.sortBy(other => branchDistance(branch, other)).tail
-      val candidates = closest.map( other => BranchConnection(branch, other) )
-      edges :::= candidates.filterNot( c => edges.exists(_ == c) || edges.exists(_.intersects(c) ) )
-    }
-    
-    object Triangle {
-      def apply(branches:List[Branch]) = {
-        branches.sortBy(_.id) match {
-          case List(a,b,c) =>
-            if(Line(a.point, b.point) ccw c.point)
-              new Triangle(a,b,c)
-            else
-              new Triangle(a,c,b)
-        }
-      }
-    }
-    case class Triangle(nA:Branch, nB:Branch, nC:Branch) {
-      require(nA != nB && nB != nC && nC != nA, "Two points are the same: %s %s %s" format(nA, nB, nC))
-      require(nA.id < nB.id && nA.id < nC.id, "Lowest Id needs to be first: %d %d %d" format(nA.id, nB.id, nC.id))
-      require(Line(nA.point, nB.point).ccw(nC.point) &&
-              Line(nB.point, nC.point).ccw(nA.point) &&
-              Line(nC.point, nA.point).ccw(nB.point), "Points not in ccw order: %s %s %s" format(nA, nB, nC) )
-      def vertices = List(nA,nB,nC)
-      def connectedTo(that:Triangle) = (this.vertices intersect that.vertices).size == 2
-      def angleAt(n:Branch) = {
-        if( n == nA ) angle(nC, nA, nB)
-        else if( n == nB ) angle(nA, nB, nC)
-        else angle(nB, nC, nA)
-      }
-      def angle(n1:Branch,n2:Branch,n3:Branch) = {
-        val a = n1.point - n2.point
-        val b = n3.point - n2.point
-        acos( (a dot b) / (a.length * b.length) )
-      }
-      def contains(p:Branch) = {
-        Line(nA.point, nB.point).ccw(p.point) &&
-        Line(nB.point, nC.point).ccw(p.point) &&
-        Line(nC.point, nA.point).ccw(p.point)
-      }
-    }
-    
-    // iterate over all connected triangles
-    def getNext:List[List[Triangle]] = {
-      val triangles = branches.flatMap(nA => nA.neighbours(edges).combinations(2).filter{
-        case List(nB,nC) => edges.exists(c => (c contains nB) && (c contains nC)) }.map{
-        case List(nB,nC) => Triangle(List(nA,nB,nC))
-      }).distinct.filterNot(t => branches.exists(t contains _))
-      
-      println(triangles.mkString("\n"))
-      
-      val connectedTriangles = triangles.combinations(2).filter{
-        case List(tA,tB) => tA connectedTo tB
-      }
-      val notMeetingDelaunayCondition = connectedTriangles.filter{
-        case List(tA,tB) =>
-          val sharedVertices = tA.vertices intersect tB.vertices
-          val angleA = tA.angleAt((tA.vertices diff sharedVertices).head)
-          val angleB = tB.angleAt((tB.vertices diff sharedVertices).head)
-          angleA + angleB > Pi
-      }
-      
-      notMeetingDelaunayCondition.toList
-    }
-    var next:List[List[Triangle]] = null
-    while( {next = getNext;next.nonEmpty} ) {
-      println(next.size)
-      val List(tA,tB) = next.head
-      val sharedVertices = tA.vertices intersect tB.vertices
-      val notSharedVertices = (tA.vertices union tB.vertices).distinct diff sharedVertices
-      edges = edges.remove{case BranchConnection(nA, nB) => (sharedVertices contains nA) && (sharedVertices contains nB) }
-      edges ::= BranchConnection(notSharedVertices(0), notSharedVertices(1))
-      next = getNext
-    }
-    
-    edges
-  }
+
   
 
   
@@ -259,14 +89,7 @@ class Dungeon(seed:Any) {
 
 
   // Connections (minimum spanning tree on complete graph)
-  var L = branches.combinations(2).collect{ case List(a,b) => BranchConnection(a,b) }.toList.sortBy{ case BranchConnection(a,b) => branchDistance(a,b) }
-  while(L.nonEmpty) {
-    val e = L.head
-    L = L.tail
-    connections ::= e
-    if(hasCycle)
-      connections = connections.tail
-  }
+
   
   // More Connections (usually creating cycles and have restrictions)
   for( branch <- branches ) {
---
+++ levelgenerator/graph.scala
@@ -0,0 +1,158 @@
+package graph
+
+trait Vertex {
+  def neighbours(edges:List[Edge]) = edges.filter(_ contains this).map(_ otherVertex this).distinct
+  def degree(edges:List[Edge]) = neighbours(edges).size
+}
+
+trait Edge {
+  def vA:Vertex
+  def vB:Vertex
+  def contains(v:Vertex) = (v == vA || v == vB)
+  def otherVertex(v:Vertex) = { require(contains(v)); if( v == vA) vB else vA }
+}
+
+trait Graph {
+  def vertices:List[Vertex]
+
+  def isConnected(edges:List[Edge]) = {
+    var visited:List[Vertex] = Nil
+    var next = vertices.take(1)
+    while( next.nonEmpty ) {
+      val current = next.head
+      next = next.tail
+      if( !(visited contains current) ) {
+        visited ::= current
+        next :::= current.neighbours(edges)
+      }
+    }
+    visited.size == edges.size
+  }
+  
+  def hasCycle(edges:List[Edge]) = {
+    var visited:List[Vertex] = Nil
+    var finished:List[Vertex] = Nil
+    var foundCycle = false
+    for( vertex <- vertices ) {
+      dfs(vertex, vertex)
+    }
+    
+    def dfs(v:Vertex, source:Vertex) {
+      if(finished contains v)
+        return
+      if((visited contains v)) {
+        foundCycle = true
+        return
+      }
+      visited ::= v
+      for( w <- v.neighbours(edges) if(w != source) )
+        dfs(w, v)
+      finished ::= v
+    }
+    
+    foundCycle
+  }
+
+}
+
+trait Triangle {
+  def a:Vertex
+  def b:Vertex
+  def c:Vertex
+}
+
+case class EuclideanVertex(point:geometry.Vec2) extends Vertex with Ordered[EuclideanVertex] {
+  import geometry._
+  def compare(that:EuclideanVertex) = this.point compare that.point
+  def distance(that:EuclideanVertex) = Line(this.point, that.point).length
+  override def neighbours(edges:List[Edge]) = super.neighbours(edges).asInstanceOf[List[EuclideanVertex]]
+}
+
+case class EuclideanEdge(vA:EuclideanVertex, vB:EuclideanVertex) extends Edge {
+  import geometry._
+  def line = Line(vA.point, vB.point)
+  def intersects(that:EuclideanEdge) = this.line sectionIntersects that.line
+}
+
+object EuclideanTriangle {
+  import geometry._
+  def apply(vertices:List[EuclideanVertex]) = {
+    vertices.sorted match {
+      case List(a,b,c) =>
+        if(Line(a.point, b.point) ccw c.point)
+          new EuclideanTriangle(a,b,c)
+        else
+          new EuclideanTriangle(a,c,b)
+      case _ => sys.error("Needed 3 Points, given: %s" format vertices)
+    }
+  }
+}
+case class EuclideanTriangle(a:EuclideanVertex, b:EuclideanVertex, c:EuclideanVertex) extends geometry.Triangle(a.point, b.point, c.point) with Triangle {
+  def contains(p:EuclideanVertex) = super.contains(p.point)
+  def vertices = List(a,b,c)
+  def angleAt(p:EuclideanVertex) = super.angleAt(p.point)
+}
+
+trait EuclideanGraph extends Graph {
+  import geometry._
+  def vertices:List[EuclideanVertex]
+
+  def minimumSpanningTree = {
+    // https://de.wikipedia.org/wiki/Algorithmus_von_Kruskal#Algorithmus
+    var edges:List[EuclideanEdge] = Nil
+    //TODO: use delaunayEdges instead of complete graph
+    var L = vertices.combinations(2).collect{ case List(a,b) => EuclideanEdge(a,b) }.toList.sortBy( _.line.length )
+    while(L.nonEmpty) {
+      val e = L.head
+      L = L.tail
+      edges ::= e
+      if(hasCycle(edges))
+        edges = edges.tail
+    }
+  }
+  
+  def delaunayEdges = {
+    var edges:List[EuclideanEdge] = Nil
+    // initial triangle mesh
+    for( vertex<- vertices ) {
+      val closest = vertices.sortBy(_ distance vertex).tail
+      val candidates = closest.map( EuclideanEdge(vertex, _) )
+      edges :::= candidates.filterNot( e => edges.exists(_ == e) || edges.exists(_ intersects e ) )
+    }
+    
+    
+    // iterate over all connected triangles
+    def getNext:List[List[EuclideanTriangle]] = {
+      val triangles = vertices.flatMap(nA => nA.neighbours(edges).combinations(2).filter{
+        case List(nB,nC) => edges.exists(c => (c contains nB) && (c contains nC)) }.collect{
+        case List(nB,nC) => EuclideanTriangle(List(nA,nB,nC))
+      }).distinct.filterNot(t => vertices.exists(t contains _))
+      
+      val connectedTriangles = triangles.combinations(2).filter{
+        case List(tA,tB) => tA connectedTo tB
+      }
+
+      val notMeetingDelaunayCondition = connectedTriangles.filter{
+        case List(tA,tB) =>
+          val sharedVertices = tA.vertices intersect tB.vertices
+          val angleA = tA.angleAt((tA.vertices diff sharedVertices).head)
+          val angleB = tB.angleAt((tB.vertices diff sharedVertices).head)
+          angleA + angleB > math.Pi
+      }
+      
+      notMeetingDelaunayCondition.toList
+    }
+    var next:List[List[EuclideanTriangle]] = null
+    while( {next = getNext;next.nonEmpty} ) {
+      println(next.size)
+      val List(tA,tB) = next.head
+      val sharedVertices = tA.vertices intersect tB.vertices
+      val notSharedVertices = (tA.vertices union tB.vertices).distinct diff sharedVertices
+      edges = edges.filterNot{case EuclideanEdge(nA, nB) => (sharedVertices contains nA) && (sharedVertices contains nB) }
+      edges ::= EuclideanEdge(notSharedVertices(0), notSharedVertices(1))
+      next = getNext
+    }
+    
+    edges
+  }
+}
